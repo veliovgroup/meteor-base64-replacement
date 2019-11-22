b84e29890d59dd8963daac1d001579d69d249030
dr.dimitru <ceo@veliov.com>
2019-11-22 04:45:40 +0300

    Use same export as used by official package

--- base64.js
+++ base64.js
@@ -1,4 +1,2 @@
 const _base64 = require('meteor/ostrio:base64').base64;
-Base64 = new _base64({ allowWebWorker: true, useNative: true, ejsonCompatible: true, supportNonASCII: false });
-
-exports.Base64 = Base64;
+export const Base64 = new _base64({ allowWebWorker: true, useNative: true, ejsonCompatible: true, supportNonASCII: false });
