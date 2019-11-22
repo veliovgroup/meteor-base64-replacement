5e04cee24c1612264e2c3bfaa76602c4237d29d0
dr.dimitru <ceo@veliov.com>
2019-11-22 04:50:05 +0300

    🚢 v1.0.15
     - 👷‍♂️ Update tests to match official Base64 package tests
     - 👷‍♂️ Update `export` to match official Base64 package export style
     - 📦 Patched version: `1.0.15`
     - 📦 Mainstream version: `1.0.12`
     - 📦 `ostrio:base64` version: `2.0.2`
    - 👷‍♂️ Regular maintenance release
    - 🤝 Compatibility with `meteor@1.8.2`

--- package.js
+++ package.js
@@ -1,21 +1,21 @@
 Package.describe({
   name: 'base64',
   summary: '[Enhanced Performance] Base64 encoding and decoding',
-  version: '1.0.14',
+  version: '1.0.15',
   git: '',
   documentation: 'README.md'
 });
 
 Package.onUse(function (api) {
   api.versionsFrom('1.5');
-  api.use(['modules', 'ostrio:base64@2.0.1'], ['client', 'server']);
   api.export('Base64');
+  api.use(['ecmascript', 'ostrio:base64@2.0.2'], ['client', 'server']);
   api.mainModule('base64.js', ['client', 'server']);
 });
 
 Package.onTest(function (api) {
   api.use('base64', ['client', 'server']);
-  api.use(['tinytest', 'underscore', 'ejson']);
+  api.use(['tinytest', 'ecmascript', 'ejson']);
 
   api.addFiles('base64_test.js', ['client', 'server']);
 });
